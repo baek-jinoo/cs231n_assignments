23ea4acbf102dbacafadc683222128ceb0b3fcfa
Jinwoo Baek <baek32jinoo@gmail.com>
2018-02-10 14:02:03 -0800

    Implement vectorized grad for softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -96,16 +96,50 @@ def softmax_loss_vectorized(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   scores = np.dot(X,W) # (500, 10)
+
+  # Shift data down (reduce magnitude) to improve numerical stability
   maxes = np.max(scores, axis=1)
   maxes = np.expand_dims(maxes, 1)
   scores -= maxes
+
   exps = np.exp(scores)
-  label_scores = exps[np.arange(len(y)), y]
+  label_exps = exps[np.arange(len(y)), y]
   sums = np.sum(exps, axis=1)
-  label_probs = label_scores / sums
-  print("probs", label_probs)
-  label_logs_probs = -np.log(label_probs)
-  print("logs", label_logs_probs)
+  probs = label_exps / sums
+  ln_probs = -np.log(probs)
+  softmax_loss = np.sum(ln_probs) / len(y)
+  loss = softmax_loss + reg * np.sum(W * W)
+
+  def dln():
+    grad_ln = 1 / probs
+    grad_negative = -1
+    grad_average = 1 / len(y)
+    
+    return grad_average * grad_negative * grad_ln
+    
+  def numerator_gradient():
+    grad_exp = label_exps
+    grad_label_exps_multiply = 1 / sums
+
+    dExpPreDot = dln() * grad_label_exps_multiply * grad_exp
+    dExp = np.zeros_like(scores)
+    dExp[np.arange(len(y)), y] = dExpPreDot
+    return np.dot(X.T, dExp)
+
+  def denominator_gradient():
+    grad_exp = exps
+    grad_flip = -1 / (sums * sums)
+    grad_label_exps_multiply = label_exps
+
+    dSum = dln() * grad_label_exps_multiply * grad_flip
+    dExp = grad_exp * np.expand_dims(dSum, 1)
+    return np.dot(X.T, dExp)
+
+  dW += denominator_gradient()
+  dW += numerator_gradient()
+
+  # Regularization gradient
+  dW += reg * 2 * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
